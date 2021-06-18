19cb2379bb6ed9510d8c38a04aaa063eba9c6d8b
Samuel Nittala <nittalasamuel7@gmail.com>
2021-06-18 18:01:09 +0530

    shorcut func

--- helper/getViper.js
+++ helper/getViper.js
@@ -1,10 +1,15 @@
 const pool = require('../db/db')
 async function getViper(details) {
-    const ability = details[1], map = details[2], area = details[3]
+    let ability = details[1], map = details[2], area = details[3]
+
+    if (ability == 'sb') ability = 'snakebite'
+    if (ability == 'pc') ability = 'poisoncloud'
+    
     const query = 
     "select url from viper where ability=$1\
-    and map=$2 and area=$3"
+    and map=$2 and $3=any(area)"
     const lineups = await pool.query(query,[ability,map,area])
+
     return lineups.rows
 }
 module.exports=getViper
